905986a3a43ba6dc68ccf7dcebd9d3393c561df7
lauraliu518 <yixuanliu0518@gmail.com>
2025-04-16 11:18:54 -0400

    bert note

--- models/bert_ner.py
+++ models/bert_ner.py
@@ -4,6 +4,11 @@ from transformers import pipeline
 class BERTNER:
     def __init__(self):
         # You can choose any model fine-tuned for NER, e.g., "dbmdz/bert-large-cased-finetuned-conll03-english"
+        # this models is:
+        # Pre-trained on large English corpus
+        # Fine-tuned on CoNLL-2003 NER dataset
+        # Cased version (preserves capitalization, important for NER)
+        # https://huggingface.co/dbmdz/bert-large-cased-finetuned-conll03-english
         self.nlp = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", aggregation_strategy="simple")
 
     def predict(self, text):
